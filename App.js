ccae9219fb0e593815313a9677ee0c415ac98878
Zehra Demir <zehrademir.gm@gmail.com>
2024-12-15 18:29:42 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import WorkoutDetailScreen from './src/screens/WorkoutDetailScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="WorkoutDetail" component={WorkoutDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
